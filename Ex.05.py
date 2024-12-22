13d262927e9c4225f04c3e4e136e4ac033de21c8
Filipe Ramos <filiperamos530@gmail.com>
2024-12-22 16:52:16 -0300

    Create Ex.05.py

---
+++ Ex.05.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um número: '))
+if num % 2 ==0:
+    print('O número é par!')
+
+else:
+    print('O  número é ímpar!')
+
+#crie um programa que leia um número inteiro qualquer e mostre na tela se ele é par ou ímpar.
